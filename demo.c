6138f5772c2903f61c3c56d021298d46fd334a75
Cyber_Karthi08 <karthikeyan9865602@gmail.com>
2024-07-22 23:57:40 +0530

    c of header

--- demo.c
+++ demo.c
@@ -6,6 +6,11 @@
 int main(){
 
     printf("The Termi Admin Name : %s & Version : %f \n", ADMIN_NAME,VERSION );
+    printf("----------------------------------------------\n");
+    const char TEAM_MEMBERS = "BHAVAN RBN";
+    const int MEMBER_ID = 01;
 
-    return 0;
+    printf("The Termin Labs team member of %u & id of %d\n",TEAM_MEMBERS, MEMBER_ID);
+
+
 }
