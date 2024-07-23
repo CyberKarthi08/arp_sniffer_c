a31382e7beb096f3bca7947d29ac021edad342ab
Cyber_Karthi08 <karthikeyan9865602@gmail.com>
2024-07-23 22:38:58 +0530

    c of define

--- demo.c
+++ demo.c
@@ -3,14 +3,20 @@
 #define ADMIN_NAME "Cyber_Karthi08"
 #define VERSION 1.1
 
+/* Declare the prototype for a general external function. */
+#ifdef REVERSIBLE_CC_MODE
+#define HAS_REVERSIBLE_CC_MODE 1
+#else
+#define HAS_REVERSIBLE_CC_MODE 0
+#endif
+
 int main(){
 
     printf("The Termi Admin Name : %s & Version : %f \n", ADMIN_NAME,VERSION );
     printf("----------------------------------------------\n");
-    const char TEAM_MEMBERS = "BHAVAN RBN";
-    const int MEMBER_ID = 01;
+    HAS_REVERSIBLE_CC_MODE = 1;
+    printf("value: %d", HAS_REVERSIBLE_CC_MODE);
 
-    printf("The Termin Labs team member of %u & id of %d\n",TEAM_MEMBERS, MEMBER_ID);
 
 
 }
